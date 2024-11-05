6c12a3cdeb3a2eb4ffb4054434f4436340342b40
demer <demer@DESKTOP-6EKA7B4>
2024-11-05 16:44:27 +0100

    Add project files.

---
+++ kajagyakorlas/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace kajagyakorlas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            fogaslb = new ListBox();
+            fogasokBindingSource = new BindingSource(components);
+            fogastxt = new TextBox();
+            Nyersanyagtxt = new TextBox();
+            Nyersanyaglb = new ListBox();
+            nyersanyagokBindingSource = new BindingSource(components);
+            labelmennyegys = new Label();
+            dataGridView1 = new DataGridView();
+            receptIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            fogasIDDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            nyersanyagNevDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            mennyiseg4foDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            egysegNevDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            árDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            hozzávalóBindingSource = new BindingSource(components);
+            Add = new Button();
+            mennyisegtxt = new TextBox();
+            remove = new Button();
+            ((System.ComponentModel.ISupportInitialize)fogasokBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nyersanyagokBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hozzávalóBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // fogaslb
+            // 
+            fogaslb.DataSource = fogasokBindingSource;
+            fogaslb.DisplayMember = "FogasNev";
+            fogaslb.FormattingEnabled = true;
+            fogaslb.ItemHeight = 15;
+            fogaslb.Location = new Point(11, 89);
+            fogaslb.Name = "fogaslb";
+            fogaslb.Size = new Size(169, 349);
+            fogaslb.TabIndex = 0;
+            fogaslb.ValueMember = "FogasID";
+            fogaslb.SelectedIndexChanged += fogaslb_SelectedIndexChanged;
+            // 
+            // fogasokBindingSource
+            // 
+            fogasokBindingSource.DataSource = typeof(Models.Fogasok);
+            // 
+            // fogastxt
+            // 
+            fogastxt.Location = new Point(11, 60);
+            fogastxt.Name = "fogastxt";
+            fogastxt.Size = new Size(169, 23);
+            fogastxt.TabIndex = 1;
+            fogastxt.TextChanged += fogastxt_TextChanged;
+            // 
+            // Nyersanyagtxt
+            // 
+            Nyersanyagtxt.Location = new Point(618, 60);
+            Nyersanyagtxt.Name = "Nyersanyagtxt";
+            Nyersanyagtxt.Size = new Size(170, 23);
+            Nyersanyagtxt.TabIndex = 2;
+            Nyersanyagtxt.TextChanged += Nyersanyagtxt_TextChanged;
+            // 
+            // Nyersanyaglb
+            // 
+            Nyersanyaglb.DataSource = nyersanyagokBindingSource;
+            Nyersanyaglb.DisplayMember = "NyersanyagNev";
+            Nyersanyaglb.FormattingEnabled = true;
+            Nyersanyaglb.ItemHeight = 15;
+            Nyersanyaglb.Location = new Point(618, 89);
+            Nyersanyaglb.Name = "Nyersanyaglb";
+            Nyersanyaglb.Size = new Size(170, 319);
+            Nyersanyaglb.TabIndex = 3;
+            Nyersanyaglb.ValueMember = "NyersanyagID";
+            Nyersanyaglb.SelectedIndexChanged += Nyersanyaglb_SelectedIndexChanged;
+            // 
+            // nyersanyagokBindingSource
+            // 
+            nyersanyagokBindingSource.DataSource = typeof(Models.Nyersanyagok);
+            // 
+            // labelmennyegys
+            // 
+            labelmennyegys.AutoSize = true;
+            labelmennyegys.Location = new Point(750, 423);
+            labelmennyegys.Name = "labelmennyegys";
+            labelmennyegys.Size = new Size(38, 15);
+            labelmennyegys.TabIndex = 4;
+            labelmennyegys.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { receptIDDataGridViewTextBoxColumn, fogasIDDataGridViewTextBoxColumn, nyersanyagNevDataGridViewTextBoxColumn, mennyiseg4foDataGridViewTextBoxColumn, egysegNevDataGridViewTextBoxColumn, árDataGridViewTextBoxColumn });
+            dataGridView1.DataSource = hozzávalóBindingSource;
+            dataGridView1.Location = new Point(195, 89);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(339, 349);
+            dataGridView1.TabIndex = 5;
+            // 
+            // receptIDDataGridViewTextBoxColumn
+            // 
+            receptIDDataGridViewTextBoxColumn.DataPropertyName = "ReceptID";
+            receptIDDataGridViewTextBoxColumn.HeaderText = "ReceptID";
+            receptIDDataGridViewTextBoxColumn.Name = "receptIDDataGridViewTextBoxColumn";
+            // 
+            // fogasIDDataGridViewTextBoxColumn
+            // 
+            fogasIDDataGridViewTextBoxColumn.DataPropertyName = "FogasID";
+            fogasIDDataGridViewTextBoxColumn.HeaderText = "FogasID";
+            fogasIDDataGridViewTextBoxColumn.Name = "fogasIDDataGridViewTextBoxColumn";
+            // 
+            // nyersanyagNevDataGridViewTextBoxColumn
+            // 
+            nyersanyagNevDataGridViewTextBoxColumn.DataPropertyName = "NyersanyagNev";
+            nyersanyagNevDataGridViewTextBoxColumn.HeaderText = "NyersanyagNev";
+            nyersanyagNevDataGridViewTextBoxColumn.Name = "nyersanyagNevDataGridViewTextBoxColumn";
+            // 
+            // mennyiseg4foDataGridViewTextBoxColumn
+            // 
+            mennyiseg4foDataGridViewTextBoxColumn.DataPropertyName = "Mennyiseg_4fo";
+            mennyiseg4foDataGridViewTextBoxColumn.HeaderText = "Mennyiseg_4fo";
+            mennyiseg4foDataGridViewTextBoxColumn.Name = "mennyiseg4foDataGridViewTextBoxColumn";
+            // 
+            // egysegNevDataGridViewTextBoxColumn
+            // 
+            egysegNevDataGridViewTextBoxColumn.DataPropertyName = "EgysegNev";
+            egysegNevDataGridViewTextBoxColumn.HeaderText = "EgysegNev";
+            egysegNevDataGridViewTextBoxColumn.Name = "egysegNevDataGridViewTextBoxColumn";
+            // 
+            // árDataGridViewTextBoxColumn
+            // 
+            árDataGridViewTextBoxColumn.DataPropertyName = "Ár";
+            árDataGridViewTextBoxColumn.HeaderText = "Ár";
+            árDataGridViewTextBoxColumn.Name = "árDataGridViewTextBoxColumn";
+            // 
+            // hozzávalóBindingSource
+            // 
+            hozzávalóBindingSource.DataSource = typeof(Hozzávaló);
+            // 
+            // Add
+            // 
+            Add.Location = new Point(549, 181);
+            Add.Name = "Add";
+            Add.Size = new Size(54, 30);
+            Add.TabIndex = 6;
+            Add.Text = "+";
+            Add.UseVisualStyleBackColor = true;
+            Add.Click += Add_Click;
+            // 
+            // mennyisegtxt
+            // 
+            mennyisegtxt.Location = new Point(618, 420);
+            mennyisegtxt.Name = "mennyisegtxt";
+            mennyisegtxt.Size = new Size(97, 23);
+            mennyisegtxt.TabIndex = 7;
+            // 
+            // remove
+            // 
+            remove.Location = new Point(549, 226);
+            remove.Name = "remove";
+            remove.Size = new Size(53, 33);
+            remove.TabIndex = 8;
+            remove.Text = "-";
+            remove.UseVisualStyleBackColor = true;
+            remove.Click += remove_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(remove);
+            Controls.Add(mennyisegtxt);
+            Controls.Add(Add);
+            Controls.Add(dataGridView1);
+            Controls.Add(labelmennyegys);
+            Controls.Add(Nyersanyaglb);
+            Controls.Add(Nyersanyagtxt);
+            Controls.Add(fogastxt);
+            Controls.Add(fogaslb);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)fogasokBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nyersanyagokBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hozzávalóBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox fogaslb;
+        private TextBox fogastxt;
+        private BindingSource fogasokBindingSource;
+        private TextBox Nyersanyagtxt;
+        private ListBox Nyersanyaglb;
+        private BindingSource nyersanyagokBindingSource;
+        private Label labelmennyegys;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn receptIDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn fogasIDDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn nyersanyagNevDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn mennyiseg4foDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn egysegNevDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn árDataGridViewTextBoxColumn;
+        private BindingSource hozzávalóBindingSource;
+        private Button Add;
+        private TextBox mennyisegtxt;
+        private Button remove;
+    }
+}
---
+++ kajagyakorlas/Form1.cs
@@ -0,0 +1,135 @@
+﻿using kajagyakorlas.Models;
+
+namespace kajagyakorlas
+{
+
+
+    public partial class Form1 : Form
+    {
+        ReceptContext _context = new ReceptContext();
+        public Form1()
+        {
+            InitializeComponent();
+            FogasokLoad();
+            NyersenyagLoad();
+            loadReceptek();
+        }
+
+
+        private void FogasokLoad()
+        {
+            var fogasok_lista = (from r in _context.Fogasok
+                                 where r.FogasNev.Contains(fogastxt.Text)
+                                 select r).ToList();
+
+            fogasokBindingSource.DataSource = fogasok_lista;
+
+
+
+        }
+
+        private void NyersenyagLoad()
+        {
+            var Nyersanyagok_lista = (from ny in _context.Nyersanyagok
+                                      where ny.NyersanyagNev.Contains(Nyersanyagtxt.Text)
+                                      select ny).ToList();
+
+            nyersanyagokBindingSource.DataSource = Nyersanyagok_lista;
+
+        }
+
+
+
+        private void fogastxt_TextChanged(object sender, EventArgs e)
+        {
+            FogasokLoad();
+        }
+
+        private void Nyersanyagtxt_TextChanged(object sender, EventArgs e)
+        {
+            NyersenyagLoad();
+        }
+
+        private void Nyersanyaglb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var kiválasztottNyersanyag = (Nyersanyagok)Nyersanyaglb.SelectedItem; //ez adja meg a listboxban kiválasztott elemet
+
+
+            if (kiválasztottNyersanyag.MennyisegiEgysegId == null) //meglesem h van-e egyáltalán ID, ha nincs --> nincs név se
+            {
+                labelmennyegys.Text = "Nincs mennyiségegység név";
+            }
+            else
+            {
+                var adottmennyisegegyseg = (from m in _context.MennyisegiEgysegek
+                                            where m.MennyisegiEgysegId == kiválasztottNyersanyag.MennyisegiEgysegId
+                                            select m).FirstOrDefault();
+
+                labelmennyegys.Text = adottmennyisegegyseg.EgysegNev;
+            }
+
+        }
+
+        private void loadReceptek()
+        {
+            var kivalasztottfogas = (Fogasok)fogaslb.SelectedItem;
+            var recept_lista = (from x in _context.Receptek
+                                where x.FogasId == kivalasztottfogas.FogasId
+                                select new Hozzávaló
+                                {
+                                    ReceptID = x.ReceptId,
+                                    FogasID = x.FogasId,
+                                    NyersanyagNev = x.Nyersanyag.NyersanyagNev,
+                                    Mennyiseg_4fo = x.Mennyiseg4fo,
+                                    EgysegNev = x.Nyersanyag.MennyisegiEgyseg.EgysegNev,
+                                    Ár = x.Mennyiseg4fo * (double?)x.Nyersanyag.Egysegar
+
+                                }).ToList();
+
+            hozzávalóBindingSource.DataSource = recept_lista;
+            dataGridView1.DataSource = hozzávalóBindingSource.DataSource;
+        }
+
+        private void fogaslb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            loadReceptek();
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            Receptek r = new Receptek();
+            r.NyersanyagId = ((Nyersanyagok)Nyersanyaglb.SelectedItem).NyersanyagId;
+            r.FogasId = ((Fogasok)fogaslb.SelectedItem).FogasId;
+            double m;
+            if (!double.TryParse(mennyisegtxt.Text, out m)) return;
+            r.Mennyiseg4fo = m;
+            _context.Receptek.Add(r);
+            _context.SaveChanges();
+            loadReceptek(); //Az középső rácsot kell újratölteni
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            var törlendő = (Hozzávaló)hozzávalóBindingSource.Current;
+
+            var törlendők = (from x in _context.Receptek
+                            where x.ReceptId== törlendő.ReceptID
+                            select x).FirstOrDefault();
+
+            _context.Receptek.Remove(törlendők);
+            _context.SaveChanges();
+            loadReceptek();
+        }
+    }
+
+    public class Hozzávaló
+    {
+        public int ReceptID { get; set; }
+        public int? FogasID { get; set; }
+        public string NyersanyagNev { get; set; }
+        public double? Mennyiseg_4fo { get; set; }
+        public string EgysegNev { get; set; }
+        public double? Ár { get; set; }
+    }
+  
+}
